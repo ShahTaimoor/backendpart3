2300a1efba101f0443dc113def340df3ae61e468
Taimoor <taimour448@gmail.com>
2024-06-01 02:04:44 +0500

    utilities added

---
+++ app.js
@@ -0,0 +1,11 @@
+// const utilities = require('./utilities')
+// const {add , sub} = utilities
+// console.log("app started");
+// console.log(add(10,5));
+// console.log(sub(10,5));
+
+
+const utilities = require('./utilities')
+let {sayHi,sayBy} = utilities
+console.log(sayHi('Taimoor'));
+console.log(sayBy('Fahad'));
---
+++ utilities.js
@@ -0,0 +1,41 @@
+// ?ways of exportin modules in csjs
+// ! single function export
+// !object export
+// ! Named function export
+
+
+
+// ?Single Function Export
+
+// const firstname = "Taimoor"
+// const greet = (name)=>{
+//     return `Hello ${name}`
+
+// }
+
+// Export This File
+
+// module.exports = greet
+
+// ?object export
+
+// module.exports = {
+//     add:function(a,b){
+// return a + b
+//     },
+//     sub:function(a,b){
+// return a - b
+//     },
+
+// }
+
+// !Named function export
+
+// module.exports.sayHi = (name)=>{
+//     return `Hi ${name}`
+// }
+
+// module.exports.sayBy = (name)=>{
+//     return `GoodBy ${name}`
+// }
+
